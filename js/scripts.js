c7730b3d3b0f9d05d41eca552aad79ede4baf5cc
Gustavo <gustavovncsl@outlook.com>
2022-10-01 20:54:17 -0300

    Ajustes e criação do script que altera dados do cartão automaticamente e simultaneamente

--- js/scripts.js
+++ js/scripts.js
@@ -26,3 +26,36 @@ btnPixPayment.addEventListener('click', () => {
     btnCard.classList.remove('method-payment-active');
     btnPix.classList.add('method-payment-active');
 });
+
+let numberCardEl = document.querySelector('.number-card');
+let dateCardEl = document.querySelector('.date-card');
+let codeCardEl = document.querySelector('.code-card');
+
+let inputNumberEl = document.querySelector('.input-number-card');
+let inputMonthEl = document.querySelector('.input-month-card');
+let inputYearEl = document.querySelector('.input-year-card');
+let inputCodeEl = document.querySelector('.input-code-card');
+
+inputNumberEl.addEventListener('input', () => {
+    resultNumber = inputNumberEl.value;
+    resultNumber = resultNumber.replace(/\D/g, " ");
+    resultNumber = resultNumber.replace(/(\d{4})/g, "$1 ");
+    numberCardEl.innerHTML = resultNumber;
+
+});
+inputYearEl.addEventListener('input', () => {
+    let monthValue = inputMonthEl.options[inputMonthEl.selectedIndex].value;
+    resultYear = inputYearEl.value;
+    resultYear = resultYear.replace(/\D/g, " ");
+    resultYear = resultYear.replace(/(\d{4})/g, "$1 ");
+    dateCardEl.innerHTML = monthValue +'/'+ resultYear;
+
+});
+inputCodeEl.addEventListener('input', () => {
+    resultCode = inputCodeEl.value;
+    resultCode = resultCode.replace(/\D/g, " ");
+    resultCode = resultCode.replace(/(\d{4})/g, "$1 ");
+    codeCardEl.innerHTML = resultCode;
+});
+
+
